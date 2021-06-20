2f2f246e6dd29409e1326bd34817515e47f2a23d
Your Name <you@example.com>
2021-06-20 23:37:24 +0530

    file commit

--- NEW PROJECTS/Room Booking/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Header = () => {
-    return (
-        <div id = "header">
-           <button className = "headbtn">Log Out</button> 
-        </div>
-    )
-}
