ed8ec817c6be99abb5d6d5fd4bae277205a3272e
Your Name <you@example.com>
2021-06-21 00:46:07 +0530

    file commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
-import React, { useState } from 'react';
-import List from './List';
+import React from 'react'
+import { useGlobalContext } from './context'
 
-const App = () =>{
-    const[value, setvalue] = useState();
-    const[items, setitems] = useState([]);
-    
-    const change = (e) =>{
-        setvalue(e.target.value);
-    }
-    
-    const btnClickedHandler =() =>{
-        if(value!==''){
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal'
+function App() {
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext()
+  if (waiting) {
+    return <SetupForm />
+  }
+  if (loading) {
+    return <Loading />
+  }
 
-            setitems((prevValue)=>{
-                return [...prevValue,value];
-            });
-            setvalue('');
-        }
-        //We empty the value so that in the input field as we click on button the input field becomes empty.
-        
-    }
-    const deleteitems = (id) =>{
-        setitems((prevValue)=>{
-            return(
-                prevValue.filter((elem,index)=>{
-                    return index!==id;
-                })
-            )
-        })
-        //Here return all the elements using filter function except the one that matches with our id that we selected. 
-    }
-    
-    return(
-        <>
-        <div className = "main_div">
-            <div className = "center_div">
-                <h1 className = "heading">ToDo list</h1>
-                <input type = "text" placeholder= "ADD ITEMS" value = {value} className = "input" onChange = {change}/>
-                <button className = "btn" onClick={btnClickedHandler} >+</button>
-                <div className ="list">
-                    {items.map((item,index)=>{
-                       return <List key={index} id ={index} text = {item} onSelect ={deleteitems}/>
-                       //give unique key to the item so that at the time of deleting the item
-                       //we know which item we are deleting. 
-                    })}
-                </div>
-            </div>
-        </div>
-        </>
-    )
+  const { question, incorrect_answers, correct_answer } = questions[index]
+  // const answers = [...incorrect_answers, correct_answer]
+  let answers = [...incorrect_answers]
+  const tempIndex = Math.floor(Math.random() * 4)
+  if (tempIndex === 3) {
+    answers.push(correct_answer)
+  } else {
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
+  return (
+    <main>
+      <Modal />
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers : {correct}/{index}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  onClick={() => checkAnswer(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              )
+            })}
+          </div>
+        </article>
+        <button className='next-question' onClick={nextQuestion}>
+          next question
+        </button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
